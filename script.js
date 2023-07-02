9898c93609ad95b6465775bc31f8c6a58d172ef8
Akila Indusara <akilaindusara@gmail.com>
2023-07-02 16:33:52 +0530

    -

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+
 //nav bar
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  let nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
   } else {
-    x.className = "topnav";
+    nav.className = "topnav";
   }
 }
 
 //theme switch
 // Retrieve the switcher element
-var switcher = document.getElementById("switcher");
+let switcher = document.getElementById("switcher");
 
 // Check if the switcher state is stored in localStorage
-var switcherState = localStorage.getItem("switcherState");
+let switcherState = localStorage.getItem("switcherState");
 
 // Set the switcher state based on the stored value (if available)
 if (switcherState === "on") {
@@ -58,6 +59,7 @@ class Tooltip extends HTMLElement {
       this.dropdown.style.right = '0';
       this.dropdown.style.transform = `translateX(${(window.outerWidth - placeholderRightX) - screenPadding}px)`;
     }
+      
   }
 
   toggle() {
@@ -102,291 +104,223 @@ document.addEventListener('touchstart', e =>   dismissAllTooltips(e));
 
 
 //Quiz
-//results
 
-var correctAnswers = 0;
-var wrongAnswers = 0;
-var ansScore = 0;
-var timeUsed = 0;
-var grade = 0;
-var seconds = 60;
-var countdown = 0;
-var performance = 0;
+//results
+let correctAnswers = 0;
+let wrongAnswers = 0;
+let ansScore = 0;
+let timeUsed = 0;
+let grade = 0;
+let countdown = 0;
+let performance = 0;
+let remainingTime;
 
 //timer
 window.onload = function() {
+    // Check if start time is stored in local storage
+    remainingTime = localStorage.getItem("remainingTime");
+
+    if (!remainingTime) {
+        // If start time is not stored, set the current time as the start time
+        remainingTime = 60;
+        localStorage.setItem("remainingTime", remainingTime);
+    }
     countdown = setInterval(function() {
-        seconds--;
-        document.getElementById("timer").innerHTML = "Time remaining: " + seconds + " seconds";
-        if (seconds == 0) {
+        remainingTime--;
+        document.getElementById("timer").innerHTML = "Time remaining: " + remainingTime + " seconds";
+        if (remainingTime == 0) {
             clearInterval(countdown);
             document.getElementById("submit-btn").click();
         }
+        localStorage.setItem("remainingTime", remainingTime);
     }, 1000);
 }
 
 // popup
-var closePopup = document.getElementById("popupclose");
-var overlay = document.getElementById("overlay");
-var popup = document.getElementById("popup");
-var button = document.getElementById("submit-btn");
+let closePopup = document.getElementById("popupclose");
+let overlay = document.getElementById("overlay");
+let popup = document.getElementById("popup");
+let subbutton = document.getElementById("submit-btn");
 // Close Popup Event
 closePopup.onclick = function() {
-	window.location.href = "Quiz_details.html";
+    localStorage.setItem("remainingTime", 60);
+    window.close();
 };
-// Show Overlay and Popup
-button.onclick = function() {
+
+// results inside popup overlay
+subbutton.onclick = function() {
     clearInterval(countdown)
-    timeUsed = 60-seconds;
+    timeUsed = 60-remainingTime;
     overlay.style.display = 'block';
     popup.style.display = 'block';
     ansScore = correctAnswers*4;
 	grade = correctAnswers*10;
+    document.getElementById("rslt").innerHTML = "<br>Questions: "+questionCount+"<br><br>Correct answers: " + correctAnswers + "<br><br>Wrong answers: " + wrongAnswers + "<br><br>Score: " + ansScore + "<br><br>Grade: " + grade +"%" + "<br><br>You took " + timeUsed + " s" + "<br><br> " ;
+    const paragraph = document.getElementById("rslt2");
 	if (grade>69){
 		performance="Excellent Mark, Keep up the good work!";
-		clz = "excellent";
+		paragraph.style.color="green";
 	} else if(grade>39){
 		performance="Good Mark, You can do better!";
-		clz = "good";
+		paragraph.style.color="orange";
 	} else {
 		performance="Bad Mark, Try harder!";
-		clz = "bad";
+		paragraph.style.color="red";
 	}
-    document.getElementById("rslt").innerHTML = "<br>Correct answers: " + correctAnswers + "<br><br>Wrong answers: " + wrongAnswers + "<br><br>Score: " + ansScore + "<br><br>Grade: " + grade +"%" + "<br><br>You took " + timeUsed + " s" + "<br><br> " ;
 	document.getElementById("rslt2").innerHTML = performance;
-	document.getElementById("rslt2").className = clz;
 }
 
+//disable button
+function disableButtons(button) {
+  const buttonContainer = button.parentNode;
+  const buttons = buttonContainer.querySelectorAll("button");
+  buttons.forEach(btn => {
+    btn.style.pointerEvents = "none";
+  });
+}
 
+//questions pool
+const questionPool = [
+  {
+    question: "Question 1",
+    answers: [
+      { id: "button1-1", text: "Answer 1" },
+      { id: "button1-2", text: "Answer 2" },
+      { id: "button1-3", text: "Answer 3" },
+      { id: "button1-4", text: "Answer 4" }
+    ],
+    correctAnswer: "button1-2"
+  },
+  {
+    question: "Question 2",
+    answers: [
+      { id: "button2-1", text: "Answer 1" },
+      { id: "button2-2", text: "Answer 2" },
+      { id: "button2-3", text: "Answer 3" },
+      { id: "button2-4", text: "Answer 4" }
+    ],
+    correctAnswer: "button2-4"
+  },
+  {
+    question: "Question 3",
+    answers: [
+      { id: "button3-1", text: "Answer 1" },
+      { id: "button3-2", text: "Answer 2" },
+      { id: "button3-3", text: "Answer 3" },
+      { id: "button3-4", text: "Answer 4" }
+    ],
+    correctAnswer: "button3-1"
+  },
+  {
+    question: "Question 4",
+    answers: [
+      { id: "button4-1", text: "Answer 1" },
+      { id: "button4-2", text: "Answer 2" },
+      { id: "button4-3", text: "Answer 3" },
+      { id: "button4-4", text: "Answer 4" }
+    ],
+    correctAnswer: "button4-2"
+  },
+  {
+    question: "Question 5",
+    answers: [
+      { id: "button5-1", text: "Answer 1" },
+      { id: "button5-2", text: "Answer 2" },
+      { id: "button5-3", text: "Answer 3" },
+      { id: "button5-4", text: "Answer 4" }
+    ],
+    correctAnswer: "button5-4"
+  },
+  // Add more questions here...
+];
 
-//answers
+// Get the questionCount from the URL query parameter
+const urlParams = new URLSearchParams(window.location.search);
+const questionCount = parseInt(urlParams.get('questionCount'));
 
-let correctAnswer1 = "button1-2";
-let correctAnswer2 = "button2-4";
-let correctAnswer3 = "button3-1";
-let correctAnswer4 = "button4-3";
-let correctAnswer5 = "button5-3";
-let correctAnswer6 = "button6-1";
-let correctAnswer7 = "button7-2";
-let correctAnswer8 = "button8-4";
-let correctAnswer9 = "button9-1";
-let correctAnswer10 = "button10-3";
+// Function to select random questions from the pool
+function selectQuestions() {
+  const poolCopy = [...questionPool];
+  const selectedQuestions = [];
 
-//answer1
-function checkAnswer1(button) {
-  if (button.id === correctAnswer1) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button1-1").style.pointerEvents = "none";
-    document.getElementById("button1-2").style.pointerEvents = "none";
-    document.getElementById("button1-3").style.pointerEvents = "none";
-    document.getElementById("button1-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button1-1").style.pointerEvents = "none";
-    document.getElementById("button1-2").style.pointerEvents = "none";
-    document.getElementById("button1-3").style.pointerEvents = "none";
-    document.getElementById("button1-4").style.pointerEvents = "none";
+  for (let i = 0; i < questionCount; i++) {
+    const randomIndex = Math.floor(Math.random() * poolCopy.length);
+    const selectedQuestion = poolCopy.splice(randomIndex, 1)[0];
+    selectedQuestions.push(selectedQuestion);
   }
 
+  return selectedQuestions;
 }
 
+// Check if the questions are already stored in sessionStorage
+const storedQuestions = sessionStorage.getItem('quizQuestions');
 
-//answer2
-function checkAnswer2(button) {
-  if (button.id === correctAnswer2) {
+if (storedQuestions) {
+  // If questions are stored, parse them and display
+  const displayedQuestions = JSON.parse(storedQuestions);
+  displayQuestions(displayedQuestions);
+} else {
+  // If questions are not stored, select new questions, store them, and display
+  const displayedQuestions = selectQuestions();
+  sessionStorage.setItem('quizQuestions', JSON.stringify(displayedQuestions));
+  displayQuestions(displayedQuestions);
+}
+
+function displayQuestions(displayedQuestions) {
+  const quizContainer = document.getElementById('quiz-container');
+  quizContainer.innerHTML = '';
+
+  for (let i = 0; i < displayedQuestions.length; i++) {
+    const question = displayedQuestions[i];
+    const questionElement = document.createElement('div');
+    questionElement.textContent = question.question;
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('btn-grid');
+
+    for (let j = 0; j < question.answers.length; j++) {
+      const answer = question.answers[j];
+      const answerButton = document.createElement('button');
+      answerButton.classList.add('btn');
+      answerButton.id = answer.id;
+      answerButton.textContent = answer.text;
+      answerButton.addEventListener('click', function() {
+        checkAnswer(answerButton, question.correctAnswer);
+      });
+
+      buttonContainer.appendChild(answerButton);
+    }
+
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(buttonContainer);
+  }
+}
+
+function checkAnswer(button, correctAnswer) {
+  if (button.id === correctAnswer) {
     button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button2-1").style.pointerEvents = "none";
-    document.getElementById("button2-2").style.pointerEvents = "none";
-    document.getElementById("button2-3").style.pointerEvents = "none";
-    document.getElementById("button2-4").style.pointerEvents = "none";
+    correctAnswers += 1;
   } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button2-1").style.pointerEvents = "none";
-    document.getElementById("button2-2").style.pointerEvents = "none";
-    document.getElementById("button2-3").style.pointerEvents = "none";
-    document.getElementById("button2-4").style.pointerEvents = "none";
+    button.style.backgroundColor = "red";
+    wrongAnswers += 1;
+
+    // Find the correct answer button and highlight it
+    const buttonContainer = button.parentNode;
+    const buttons = buttonContainer.querySelectorAll("button");
+    buttons.forEach((btn) => {
+      if (btn.id === correctAnswer) {
+        btn.style.backgroundColor = "green";
+      }
+      btn.style.pointerEvents = "none";
+    });
   }
 
+  disableButtons(button);//disable buttons after selecting a question
 }
 
-
-
-//answer3
-function checkAnswer3(button) {
-  if (button.id === correctAnswer3) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button3-1").style.pointerEvents = "none";
-    document.getElementById("button3-2").style.pointerEvents = "none";
-    document.getElementById("button3-3").style.pointerEvents = "none";
-    document.getElementById("button3-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button3-1").style.pointerEvents = "none";
-    document.getElementById("button3-2").style.pointerEvents = "none";
-    document.getElementById("button3-3").style.pointerEvents = "none";
-    document.getElementById("button3-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer4
-function checkAnswer4(button) {
-  if (button.id === correctAnswer4) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button4-1").style.pointerEvents = "none";
-    document.getElementById("button4-2").style.pointerEvents = "none";
-    document.getElementById("button4-3").style.pointerEvents = "none";
-    document.getElementById("button4-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button4-1").style.pointerEvents = "none";
-    document.getElementById("button4-2").style.pointerEvents = "none";
-    document.getElementById("button4-3").style.pointerEvents = "none";
-    document.getElementById("button4-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer5
-function checkAnswer5(button) {
-  if (button.id === correctAnswer5) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button5-1").style.pointerEvents = "none";
-    document.getElementById("button5-2").style.pointerEvents = "none";
-    document.getElementById("button5-3").style.pointerEvents = "none";
-    document.getElementById("button5-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button5-1").style.pointerEvents = "none";
-    document.getElementById("button5-2").style.pointerEvents = "none";
-    document.getElementById("button5-3").style.pointerEvents = "none";
-    document.getElementById("button5-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer6
-function checkAnswer6(button) {
-  if (button.id === correctAnswer6) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button6-1").style.pointerEvents = "none";
-    document.getElementById("button6-2").style.pointerEvents = "none";
-    document.getElementById("button6-3").style.pointerEvents = "none";
-    document.getElementById("button6-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button6-1").style.pointerEvents = "none";
-    document.getElementById("button6-2").style.pointerEvents = "none";
-    document.getElementById("button6-3").style.pointerEvents = "none";
-    document.getElementById("button6-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer7
-function checkAnswer7(button) {
-  if (button.id === correctAnswer7) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button7-1").style.pointerEvents = "none";
-    document.getElementById("button7-2").style.pointerEvents = "none";
-    document.getElementById("button7-3").style.pointerEvents = "none";
-    document.getElementById("button7-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button7-1").style.pointerEvents = "none";
-    document.getElementById("button7-2").style.pointerEvents = "none";
-    document.getElementById("button7-3").style.pointerEvents = "none";
-    document.getElementById("button7-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer8
-function checkAnswer8(button) {
-  if (button.id === correctAnswer8) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button8-1").style.pointerEvents = "none";
-    document.getElementById("button8-2").style.pointerEvents = "none";
-    document.getElementById("button8-3").style.pointerEvents = "none";
-    document.getElementById("button8-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button8-1").style.pointerEvents = "none";
-    document.getElementById("button8-2").style.pointerEvents = "none";
-    document.getElementById("button8-3").style.pointerEvents = "none";
-    document.getElementById("button8-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer9
-function checkAnswer9(button) {
-  if (button.id === correctAnswer9) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button9-1").style.pointerEvents = "none";
-    document.getElementById("button9-2").style.pointerEvents = "none";
-    document.getElementById("button9-3").style.pointerEvents = "none";
-    document.getElementById("button9-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button9-1").style.pointerEvents = "none";
-    document.getElementById("button9-2").style.pointerEvents = "none";
-    document.getElementById("button9-3").style.pointerEvents = "none";
-    document.getElementById("button9-4").style.pointerEvents = "none";
-  }
-
-}
-
-
-
-//answer10
-function checkAnswer10(button) {
-  if (button.id === correctAnswer10) {
-    button.style.backgroundColor = "green";
-	correctAnswers +=1;
-    document.getElementById("button10-1").style.pointerEvents = "none";
-    document.getElementById("button10-2").style.pointerEvents = "none";
-    document.getElementById("button10-3").style.pointerEvents = "none";
-    document.getElementById("button10-4").style.pointerEvents = "none";
-  } else {
-    button.style.backgroundColor = "red";	
-	wrongAnswers +=1;
-    document.getElementById("button10-1").style.pointerEvents = "none";
-    document.getElementById("button10-2").style.pointerEvents = "none";
-    document.getElementById("button10-3").style.pointerEvents = "none";
-    document.getElementById("button10-4").style.pointerEvents = "none";
-  }
-
-}
-
-
+//get number of questions from user and redirect
+document.getElementById("quizForm").onclick = (function(event) {
+  event.preventDefault();
+  var questionCount = document.getElementById("qCount").value;
+  window.location.href = "Quiz.html?questionCount=" + questionCount;
+});
